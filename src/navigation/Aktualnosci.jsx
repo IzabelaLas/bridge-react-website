3cb434e0c7cfe9544509cd7d7a93b087daaacc7c
Izabela <izabela.laszczuk@gmail.com>
2023-05-11 16:03:45 +0200

    Add aktualnosci component layout

--- src/navigation/Aktualnosci.jsx
+++ src/navigation/Aktualnosci.jsx
@@ -1,27 +1,18 @@
+import cardsPicture from '@/images/cardsPicture.jpg'
+
 export default function Aktualnosci() {
     return (
         <div className="text-center">
-          <div>
-            Witamy w  Aktualnościach
-           </div>
-          <p>Tu mozemy zobaczyć co będzie widocznie w Aktualności </p>
-          <div>
-            Witamy w  Aktualności
-           </div>
-          <p>Tu mozemy zobaczyć co będzie widocznie w Aktualności </p><div>
-            Witamy w  Aktualności
-           </div>
-          <p>Tu mozemy zobaczyć co będzie widocznie w Aktualności </p><div>
-            Witamy w  Aktualności
-           </div>
-          <p>Tu mozemy zobaczyć co będzie widocznie w Aktualności </p><div>
-            Witamy w  Aktualności
-           </div>
-          <p>Tu mozemy zobaczyć co będzie widocznie w Aktualności </p><div>
-            Witamy w  Aktualności
-           </div>
-          <p>Tu mozemy zobaczyć co będzie widocznie w Aktualności </p>
-       
+          <h2 className="font-bold text-4xl tracking-tight text-gray-900 sm:text-6xl md:text-6xl lg:text-6xl xl:text-6xl 2xl:text-6xl">Aktualności</h2>
+          <div className="items-center grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2">
+              <div className='m-4 p-2 sm:p-5 md:m-6 md:p-5 lg:p-8 xl:m-10 xl:p-10'>
+                <img src={cardsPicture} alt="Zdjęcie kart w ręce kobiety" />
+              </div>
+              <div className='m-4 p-2 sm:p-5 md:m-6 md:p-5 lg:p-8'>
+                <h3 className='text-2xl font-bold mb-2 tracking-tight text-gray-900 sm:text-2xl'>Lorem ipsum dolor sit,</h3>
+                <p className='p-2 text-[15px] xs:text-[10px] sm:text-[11px] md:text-sm lg:text-[19px]'> amet consectetur adipisicing elit. Ipsum quasi sunt, voluptates odit vitae laborum veritatis officiis ea, unde ducimus praesentium. Consequatur quo et nesciunt blanditiis corporis sed doloremque fuga! amet consectetur adipisicing elit. Ipsum quasi sunt, voluptates odit vitae laborum veritatis officiis ea, unde ducimus praesentium. Consequatur quo et nesciunt blanditiis corporis sed doloremque fuga! amet consectetur adipisicing elit. Ipsum quasi sunt, voluptates odit vitae laborum veritatis officiis ea, unde ducimus praesentium. Consequatur quo et nesciunt blanditiis corporis sed doloremque fuga! amet consectetur adipisicing elit. Ipsum quasi sunt, voluptates odit vitae laborum veritatis officiis ea, unde ducimus praesentium. Consequatur quo et nesciunt blanditiis corporis sed doloremque fuga! amet consectetur adipisicing elit. Ipsum quasi sunt, voluptates odit vitae laborum veritatis officiis ea, unde ducimus praesentium. Consequatur quo et nesciunt blanditiis corporis sed doloremque fuga!</p>
+              </div>
+          </div>
         </div>
     )
 }
--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -43,8 +43,8 @@ export default function Navigation() {
     <div>
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
+        <nav className="flex items-center justify-between p-6 lg:px-8 xl:px-8" aria-label="Global">
+          <div className="flex lg:flex-1 xl:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Brydz Sieradz</span>
               <img
@@ -54,7 +54,7 @@ export default function Navigation() {
               />
             </a>
           </div>
-          <div className="flex lg:hidden">
+          <div className="flex lg:hidden xl:hidden">
             <button
               type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -64,22 +64,22 @@ export default function Navigation() {
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+          <div className="hidden lg:flex lg:gap-x-12 xl:flex xl:gap-x-12">
             {navigation.map(({ name, href, value }) => (
               <a key={name} href={href} onClick={(event) => handleClick(event, value)} className="text-sm font-semibold leading-6 text-gray-900">
                 {name} 
               </a>
             ))}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end xl:flex xl:flex-1 xl:justify-end">
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
               Link <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog as="div" className="lg:hidden xl:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <Dialog.Panel className="fixed top-0 inset-y-80 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
@@ -142,7 +142,7 @@ export default function Navigation() {
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl md:text-6xl lg:text-6xl xl:text-6xl 2xl:text-6xl">
               Data to enrich your online business
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
@@ -167,7 +167,7 @@ export default function Navigation() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#0E9F4C] to-[#00662B] opacity-50 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[20.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#0E9F4C] to-[#00662B] opacity-50 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
