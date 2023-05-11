5efdbff9202c7cc56df052c691dcab47bfde31c6
Izabela <izabela.laszczuk@gmail.com>
2023-05-11 16:03:56 +0200

    Add team layout

--- src/navigation/Team.jsx
+++ src/navigation/Team.jsx
@@ -1,52 +1,43 @@
-
+import people from '@/images/people.jpg'
 
 export default function Team() {
     return (
-        <div className="text-center"> 
+      <div className="text-center">
+        <h2 className="font-bold text-4xl tracking-tight text-gray-900 sm:text-6xl md:text-6xl lg:text-6xl xl:text-6xl 2xl:text-6xl">Team members</h2>
+        <div className="grid grid-cols-2 gap-1 m-1"> 
           <div>
-            Hello from Team: 
-           </div>
-          <p>This is a text for testing TEAM </p>
+            <img src={people} alt="team member" />
+            <p>John, Warsaw</p>
+          </div>
+           <div>
+            <img src={people} alt="team member" />
+            <p>Mark, Warsaw</p>
+          </div>
           <div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p><div>
-            Hello from Team
-           </div>
-          <p>This is a text for testing TEAM </p>
+            <img src={people} alt="team member" />
+            <p>Amy, Warsaw</p>
+          </div>
+          <div>
+            <img src={people} alt="team member" />
+            <p>Bob, Warsaw</p>
+          </div>
+          <div>
+            <img src={people} alt="team member" />
+            <p>John, Warsaw</p>
+          </div>
+           <div>
+            <img src={people} alt="team member" />
+            <p>John, Warsaw</p>
+          </div>
+          <div>
+            <img src={people} alt="team member" />
+            <p>John, Warsaw</p>
+          </div>
+          <div>
+            <img src={people} alt="team member" />
+            <p>John, Warsaw</p>
+          </div>
+        </div>
         </div>
     )
 }
