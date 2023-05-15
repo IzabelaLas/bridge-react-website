fd5fec5afa7f102d4f475a03d7e4bc37028e26cc
Izabela <izabela.laszczuk@gmail.com>
2023-05-15 10:36:45 +0200

    Add map() to team

--- src/navigation/Team.jsx
+++ src/navigation/Team.jsx
@@ -1,42 +1,37 @@
 import people from '@/images/people.jpg'
+import people2 from '@/images/people.jpg'
+import people3 from '@/images/people.jpg'
+import people4 from '@/images/people.jpg'
+import people5 from '@/images/people.jpg'
+import people6 from '@/images/people.jpg'
+import people7 from '@/images/people.jpg'
+import people8 from '@/images/people.jpg'
+
 
 export default function Team() {
+
+   const pictures = [
+  { foto: people, name: 'John, Warsaw' },
+  { foto: people2, name: 'Mark, Warsaw' },
+  { foto: people3, name: 'Bob, Warsaw' },
+  { foto: people4, name: 'Amy, Warsaw' },
+  { foto: people5, name: 'John, Warsaw' },
+  { foto: people6, name: 'Mark, Warsaw' },
+  { foto: people7, name: 'Bob, Warsaw' },
+  { foto: people8, name: 'Amy, Warsaw' },
+];
+
     return (
       <div className="text-center">
         <h2 className="font-bold text-4xl tracking-tight text-gray-900 sm:text-6xl md:text-6xl lg:text-6xl xl:text-6xl 2xl:text-6xl">Team members</h2>
         <div className="grid grid-cols-2 gap-1 m-1"> 
-          <div>
-            <img src={people} alt="team member" />
-            <p>John, Warsaw</p>
+
+        {pictures.map((picture, index)  => (
+          <div key={index}>
+               <img src={picture.foto} alt="team member" />
+               <p>{picture.name}</p>
           </div>
-           <div>
-            <img src={people} alt="team member" />
-            <p>Mark, Warsaw</p>
-          </div>
-          <div>
-            <img src={people} alt="team member" />
-            <p>Amy, Warsaw</p>
-          </div>
-          <div>
-            <img src={people} alt="team member" />
-            <p>Bob, Warsaw</p>
-          </div>
-          <div>
-            <img src={people} alt="team member" />
-            <p>John, Warsaw</p>
-          </div>
-           <div>
-            <img src={people} alt="team member" />
-            <p>John, Warsaw</p>
-          </div>
-          <div>
-            <img src={people} alt="team member" />
-            <p>John, Warsaw</p>
-          </div>
-          <div>
-            <img src={people} alt="team member" />
-            <p>John, Warsaw</p>
-          </div>
+        ))}
         </div>
         </div>
     )
