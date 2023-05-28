3d5e4aef94876edcccab2f327b95322ca0bb1ff5
Izabela <izabela.laszczuk@gmail.com>
2023-05-28 20:10:55 +0200

    Add footer

---
+++ src/navigation/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className="text-pink-300 p-[80px] text-center">
+           <span>Website made by</span>
+           <span className=""><a href="https://github.com/IzabelaLas" className="text-pink-600"> Izabela Ł.</a></span>
+           
+        </div>
+    )
+}
--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -5,7 +5,8 @@ import bridgeLogo from '@/poker-cards.png'
 import Team from '@/navigation/Team'
 import Aktualnosci from '@/navigation/Aktualnosci'
 import Wydarzenia from '@/navigation/Wydarzenia'
-import Wyniki from './Wyniki'
+import Wyniki from '@/navigation/Wyniki'
+import Footer from '@/navigation/Footer'
 
 const navigation = [
   { name: 'Aktualności', href: '#aktualnosciTarget', value: 'aktualnosciTargetRef'  },
@@ -72,8 +73,8 @@ export default function Navigation() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end xl:flex xl:flex-1 xl:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Link <span aria-hidden="true">&rarr;</span>
+            <a href="https://www.pzbs.pl/" target="_blank" rel="noreferrer" className="text-sm font-semibold leading-6 text-gray-900">
+              PZBS <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
@@ -141,24 +142,13 @@ export default function Navigation() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           </div>
-          <div className="text-center">
+          <div className="text-center mb-20 mt-10">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl md:text-6xl lg:text-6xl xl:text-6xl 2xl:text-6xl">
               Brydż sportowy w Sieradzu
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Znajdą tu Państwo najważniejsze informacje dotyczące brydża i dokonań naszej drużyny. Zapraszamy serdecznie do zapoznania się z aktualnościami.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-[#00662B] px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
           </div>
         </div>
         <div
@@ -186,6 +176,9 @@ export default function Navigation() {
      </div> 
      <div ref={wynikiTargetRef} id="wynikiTarget">
       <Wyniki />
+     </div>
+      <div>
+      <Footer />
      </div>
     </div>
   )
